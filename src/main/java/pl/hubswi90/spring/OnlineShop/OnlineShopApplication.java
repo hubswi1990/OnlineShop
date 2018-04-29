e85806299bead93cae372e1ef31b7fa02097c6c4
hubswi90 <hubswi90@gmail.com>
2018-04-29 12:33:49 +0200

    init project

---
+++ src/main/java/pl/hubswi90/spring/OnlineShop/OnlineShopApplication.java
@@ -0,0 +1,18 @@
+package pl.hubswi90.spring.OnlineShop;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopApplication implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopApplication.class, args);
+	}
+}
